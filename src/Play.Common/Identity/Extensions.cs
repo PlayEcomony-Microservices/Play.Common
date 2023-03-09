18aa4ef84eb0f3fea4366f19d3bfc87c1335e707
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-03-09 20:08:27 +0300

    add classes to allow simplified way of configuring authentication in microservices

---
+++ src/Play.Common/Identity/Extensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace Play.Common.Identity
+{
+    public static class Extensions
+    {
+        public static AuthenticationBuilder AddJwtBearerAuthenticaton(this IServiceCollection services)
+        {
+            return services.ConfigureOptions<ConfigureJwtBearerOptions>()
+                            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                            .AddJwtBearer(); 
+        }
+    }
+}
