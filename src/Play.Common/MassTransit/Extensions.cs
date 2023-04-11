dfdc8dae99784f53660e31dff921dc27ca335233
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-04-11 22:10:30 +0300

    update comon library by configuring retries

--- src/Play.Common/MassTransit/Extensions.cs
+++ src/Play.Common/MassTransit/Extensions.cs
@@ -7,36 +7,42 @@ using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Play.Common.Settings;
+using MassTransit.ExtensionsDependencyInjectionIntegration;
 
 namespace Play.Common.MassTransit
 {
     public static class Extensions
     {
-        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services)
+        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services, Action<IRetryConfigurator> configureRetries = null)
         {
-            
+
             services.AddMassTransit(configure =>
             {
                 configure.AddConsumers(Assembly.GetEntryAssembly());
-
-
-                configure.UsingRabbitMq((context, configurator) =>
-                {
-                    var configuration = context.GetService<IConfiguration>();
-                    var serviceSettings = configuration?.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-                    var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-                    configurator.Host(rabbitMQSettings.Host);
-                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-
-                    //enable message retry
-                    configurator.UseMessageRetry(retryConfigurator =>
-                    {
-                        retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
-                    });
-                });
+                configure.UsingPlayEconomyRabbitMq(configureRetries);
             });
 
             return services;
+        }
+
+        public static void UsingPlayEconomyRabbitMq(this IServiceCollectionBusConfigurator configure, Action<IRetryConfigurator> configureRetries = null)
+        {
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                var configuration = context.GetService<IConfiguration>();
+                var serviceSettings = configuration?.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                configurator.Host(rabbitMQSettings.Host);
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+
+                if (configureRetries is null)
+                {
+                    configureRetries = (retryConfigurator) =>
+                        retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                }
+                //enable message retry
+                configurator.UseMessageRetry(configureRetries);
+            });
         }
     }
 }
